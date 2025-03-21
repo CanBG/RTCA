75ba053f5b8e9caccc70fe560940e405d11de514
CanBG <cansarper1@hotmail.com>
2025-03-21 13:42:38 +0300

    b2

---
+++ backend/src/models/message.model.js
@@ -0,0 +1,28 @@
+import mongoose, { mongo } from "mongoose";
+import User from "./user.model.js";
+
+const messageSchema = new mongoose.Schema(
+    {
+        senderId:{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: User,
+            required: true,
+        },
+        senderId:{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: User,
+            required: true,
+        },
+        text:{
+            type: String,
+        },
+        image:{
+            type: String,
+        },
+    },
+    {timestamps: true}
+);
+
+const Message = mongoose.model("Message",messageSchema);
+
+export default Message;
